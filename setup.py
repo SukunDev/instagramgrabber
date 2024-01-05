178b898e54923094308467aaec96febccebb99fe
SukunDev <sukundev32@gmail.com>
2024-01-05 11:25:38 +0700

    change

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
     keywords=["instagram", "instagram-grabber", "instagram grabber", "python3", "api", "instagram api"],
     install_requires=["requests"],
     classifiers=[
-        "Development Status :: 1 - Beta",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Build Tools",
         "License :: OSI Approved :: MIT License",
